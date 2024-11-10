7e458cc7950a96a155d96ad881006804c521b28d
Rohit Sardar <39742697+RohitSardar@users.noreply.github.com>
2024-11-10 01:32:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! Welcome to your Flask App running in a Docker container!"
+    return '''  <html>
+                <head>
+                <title>CI/CD</title>
+                </head>
+                <body>
+                <h1> Hello World, I am a flask app running in a docker container. </h1>
+                </body>
+                </html>'''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
